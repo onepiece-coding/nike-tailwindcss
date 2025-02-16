2b12bafdc7635965a5cfcebdaf37a96da8a9680f
onepiece-coding <makclaaax@gmail.com>
2025-02-16 05:39:19 +0100

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import SpeciaLOffer from "./SpeciaLOffer";
+import CustomerReviews from "./CustomerReviews";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+
+export {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  SpeciaLOffer,
+  CustomerReviews,
+  Subscribe,
+  Footer,
+};
